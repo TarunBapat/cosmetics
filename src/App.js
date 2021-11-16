ef1dd71865811c8bbe58ddb3c87d2ae9b4b160b2
TarunBapat <32245027+TarunBapat@users.noreply.github.com>
2021-11-16 22:55:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./styles.css";
 import About from "./components/About";
 import Store from "./components/Store";
+import Home from "./components/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
-    
+    <BrowserRouter>
       <div className="App">
-        <Route path="/about" component={About} />
-        <Route path="/store" component={Store} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/store" exact component={Store} />
+        </Switch>
       </div>
-    
+    </BrowserRouter>
   );
 }
 
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,6 +1,27 @@
-const Store=()=>{
-  return(
-    <h1>Store</h1>
-  )
-}
+import { useState, useEffect } from "react";
+
+const Store = () => {
+  const [products,setProducts]=useState([]);
+  useEffect(() => {
+    fetchCosmetics();
+  }, []);
+  const fetchCosmetics = async () => {
+    const data = await fetch("https://fortnite-api.com/v2/cosmetics/br/");
+    const resp = await data.json();
+    setProducts(resp.data);
+    console.log(resp.data)
+  };
+  return (
+    <div>
+    {products.map((product)=>{
+      return(
+        <div>
+          <img src={product.images.icon} />
+        <h1>{product.name}</h1>
+        </div>
+      )
+    })}
+    </div>
+  );
+  }
 export default Store;
